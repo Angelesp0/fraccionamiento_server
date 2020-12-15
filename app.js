e5db9d92578a76e087ad99028f04ad5ac32c6eca
Angelesp0 <angel-ed-s@hotmail.com>
2020-12-14 20:04:52 -0700

    Conexion a BD de AWS

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.send('cors problem fixed:)');
+
     res.json({ message: "bienvenido a fraccionamiento-app" });
 });
 
