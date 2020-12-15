0b6722793f424c78e7576f828616b606e2370700
Angelesp0 <angel-ed-s@hotmail.com>
2020-12-14 20:16:42 -0700

    Conexion a BD de AWS

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sql = require('./app/models/db.js');
-var cors = require('cors')
+var cors = require('cors');
 const path = require('path');
 const session = require('express-session');
 const config = require('./app/config/jwt.config');
 
 const app = express();
 
-var whitelist = ['http://localhost:4200', 'http://fraccionamiento.herokuapp.com']
-var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
-
-app.use(cors(corsOptions))
+app.use(cors({origin: '*'}));
 app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
@@ -27,12 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    res.send('cors problem fixed:)');
-
     res.json({ message: "bienvenido a fraccionamiento-app" });
 });
 
