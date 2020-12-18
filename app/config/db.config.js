97037c06988aef250700e7f54abef2dc1b527508
Angelesp0 <angel-ed-s@hotmail.com>
2020-12-18 00:44:32 -0700

    arreglos servicios

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    host: 'bd-fraccionamiento.cs0pxqkd2he1.us-east-2.rds.amazonaws.com',
+    /*host: 'bd-fraccionamiento.cs0pxqkd2he1.us-east-2.rds.amazonaws.com',
     user: 'angelesp0',
     password: 'Megamanzero123$',
     database: 'fraccionamiento',
-    port: 3306
+    port: 3306*/
     // aplicacion en local //
-   /* host: '192.168.100.80',
+    host: '192.168.100.80',
     user: 'frac',
     password: 'root',
     database: 'fraccionamiento',
-    port: 3306 */
+    port: 3306 
 };
